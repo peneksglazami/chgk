2c0a18727c93245ca4a78cf35c9985cd6ca9c9f1
grand <peneksglazami@gmail.com>
2013-07-12 23:27:32 +0400

    Реализация операции авторизации.

--- src/org/cherchgk/security/realms/HibernateRealm.java
+++ src/org/cherchgk/security/realms/HibernateRealm.java
@@ -2,15 +2,20 @@ package org.cherchgk.security.realms;
 
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.codec.Hex;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
+import org.cherchgk.domain.security.Permission;
+import org.cherchgk.domain.security.Role;
 import org.cherchgk.domain.security.User;
 import org.cherchgk.utils.EntityManagerProvider;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Andrey Grigorov (peneksglazami@gmail.com)
@@ -52,6 +57,27 @@ public class HibernateRealm extends AuthorizingRealm {
 
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-        return null;
+        String username = (String) getAvailablePrincipal(principals);
+        User user = entityManager.createQuery("select user from User user where user.username = :username", User.class)
+                .setParameter("username", username)
+                .getSingleResult();
+
+        Set<String> roleNames = new HashSet<String>();
+        Set<String> permissions = new HashSet<String>();
+
+        for (Role role : user.getRoles()) {
+            roleNames.add(role.getName());
+            for (Permission permission : role.getPermissions()) {
+                permissions.add(permission.getPermission());
+            }
+        }
+
+        for (Permission permission : user.getPermissions()) {
+            permissions.add(permission.getPermission());
+        }
+
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo(roleNames);
+        info.setStringPermissions(permissions);
+        return info;
     }
 }
