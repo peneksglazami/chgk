34ba80ef20291bd85d985f9a5711acdef3efb871
grand <peneksglazami@gmail.com>
2013-11-17 03:27:30 +0400

    Добавлена проверка прав на выполнение операции изменения признака ответа на вопрос.

--- src/org/cherchgk/actions/tournament/result/EditAnswerVerdictAction.java
+++ src/org/cherchgk/actions/tournament/result/EditAnswerVerdictAction.java
@@ -2,11 +2,13 @@ package org.cherchgk.actions.tournament.result;
 
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
+import org.cherchgk.domain.Team;
+import org.cherchgk.security.PermissionChecker;
 import org.cherchgk.services.TeamService;
 import org.cherchgk.utils.ActionContextHelper;
 
 /**
- * Действие изменения результата ответа на вопрос
+ * Действие изменения результата ответа на вопрос.
  *
  * @author Andrey Grigorov (peneksglazami@gmail.com)
  */
@@ -21,6 +23,8 @@ public class EditAnswerVerdictAction extends ActionSupport {
     @Override
     public String execute() throws Exception {
         long teamId = Long.valueOf(ActionContextHelper.getRequestParameterValue("teamId"));
+        Team team = teamService.find(teamId);
+        PermissionChecker.checkPermissions("tournament:edit:" + team.getTournament().getId());
         int questionNumber = Integer.valueOf(ActionContextHelper.getRequestParameterValue("questionNumber"));
         int verdict = Integer.valueOf(ActionContextHelper.getRequestParameterValue("verdict"));
         teamService.setAnswerVerdict(teamId, questionNumber, verdict == 1);
