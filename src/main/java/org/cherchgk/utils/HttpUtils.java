0d4f688a2b7d5fb94abaf6d1f0055a35f60bda11
grand <peneksglazami@gmail.com>
2015-03-01 15:32:53 +0300

    Удалена ненужная проверка в HttpUtils.replaceToHex.

--- src/main/java/org/cherchgk/utils/HttpUtils.java
+++ src/main/java/org/cherchgk/utils/HttpUtils.java
@@ -67,7 +67,7 @@ public final class HttpUtils {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            if ((c >= 0) && (c <= 255) && !Character.isWhitespace(c)) {
+            if ((c <= 255) && !Character.isWhitespace(c)) {
                 sb.append(c);
             } else {
                 byte[] bytes = Character.toString(c).getBytes("UTF-8");
